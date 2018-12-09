72aede358ebff1e13b9e81a31b24d74c56864343
Austin Andrews <admin@templarian.com>
2018-12-08 18:56:18 -0600

    Updates to support `@mdi/light-svg`.

--- util.js
+++ util.js
@@ -1,25 +1,40 @@
 const fs = require('fs');
 
 const encoding = "utf8";
-const folder = `${__dirname}/../svg`;
+const folder = `${__dirname}/../`;
+const packageName = 'svg'
 
-exports.getVersion = () => {
-  const file = fs.readFileSync(`${folder}/package.json`, { encoding });
+const getVersion = (overridePackageName) => {
+  const pName = overridePackageName || packageName;
+  const file = fs.readFileSync(`${folder}${pName}/package.json`, { encoding });
   return JSON.parse(file).version;
 };
 
-exports.getMeta = (withPaths) => {
-  const file = fs.readFileSync(`${folder}/meta.json`, { encoding });
+const getMeta = (withPaths, overridePackageName) => {
+  const pName = overridePackageName || packageName;
+  const file = fs.readFileSync(`${folder}${pName}/meta.json`, { encoding });
   const meta = JSON.parse(file);
   if (withPaths) {
     const total = meta.length;
     meta.forEach((icon, i) => {
-      const svg = fs.readFileSync(`${folder}/svg/${icon.name}.svg`, { encoding });
+      const svg = fs.readFileSync(`${folder}${pName}/svg/${icon.name}.svg`, { encoding });
       icon.path = svg.match(/d="([^"]+)"/)[1];
     });
   }
   return meta;
 };
+
+exports.getVersionLight = () => {
+  return getVersion('light-svg');
+}
+
+exports.getMetaLight = (withPaths) => {
+  return getMeta(withPaths, 'light-svg');
+}
+
+exports.getVersion = getVersion;
+
+exports.getMeta = getMeta;
 
 exports.closePath = (path) => {
   return path.replace(/(\d)M/g, '$1ZM');
